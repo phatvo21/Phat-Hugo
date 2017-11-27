60ca700d7f068c608a665195a88daec5d0403d8f
Hugo <hugo@Jasons-MBP.net.fpt>
2017-11-27 16:22:32 +0700

    Added link styling, one page scroll, and placeholder divs for projects.

---
+++ resources/scripts/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({verticalCentered: false});
+
+    $('.red').click(function() {
+
+        $(this).toggleClass('black');
+
+    })
+
+    $('.blue').click(function() {
+
+        $(this).toggleClass('black');
+
+    })
+
+    $('.green').click(function() {
+
+        $(this).toggleClass('black');
+
+    })
+
+
+});
+
